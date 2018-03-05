689afd6d6828a25e595ed03318127ee6ddad8c46
Ed M <exm8563@rit.edu>
2018-03-05 14:41:47 -0500

    velocity-to-DAC-value converter implemented; transmitState implemented (with dummy names for DB things, for now); changed genCalMap slightly, I think additive adjustment might be better; RCLK is now one pulse at the end of the serial write; added JSDoc comments

--- spicomm.js
+++ spicomm.js
@@ -2,6 +2,7 @@
 // Author: Edward Maskelony     exm8563@rit.edu
 
 var rpio = require('rpio');
+var db = require('lokijs');
 
 // Pin mapping (physical, not BCOM):
 //
@@ -39,6 +40,7 @@ const adc_cmd = {  // includes DAC opcodes in case we need them
     "dataReset":0xE000,
     "dataCtrlReset":0xF000
 };
+const solenoid_on_time_limit = 8; // seconds that the solenoid is allowed to be active for
 
 exports.initSpi = function () {
     var initsettings = {
@@ -62,8 +64,10 @@ exports.initSpi = function () {
     // rpio.spiChipSelect(0); // we only need this if we return to using SPI CE pins as RCLK, also need to consider setCSPolarity()
 };
 
+/**
+ * turn off all solenoids, disable SPI mode, set GPIO low on relevant pins
+ */
 exports.finishSpi = function() {
-    // disable SPI mode, set GPIO low on relevant pins, turn off all solemoids
     setKeyEnables([]);
     for (var x=1; x <= modules_connected; x++){
         rpio.write(board_enables[x], rpio.LOW);
@@ -72,20 +76,30 @@ exports.finishSpi = function() {
     rpio.spiEnd();
 };
 
+/**
+ * Set RCLK 0 to 1, ready for the next rising edge
+ */
 function rclkRisingEdge() {
     // Set RCLK 0 to 1, pushing data in serial registers to output registers
     //rpio.spiChipSelect(0);
     rpio.write(shiftreg_rclk, rpio.HIGH);
 }
 
+/**
+ * Set RCLK 1 to 0, pushing data in serial registers to output registers
+ */
 function rclkFallingEdge() {
     // Set RCLK 1 to 0, pushing data in serial registers to output registers
     //rpio.spiChipSelect(1); // CE0 selected,
     rpio.write(shiftreg_rclk, rpio.LOW);
 }
 
+/**
+ * Send SPI command to change the DAC voltage
+ * @param {int} keyAddress - MIDI number of the key to be updated
+ * @param {int} value - 10-bit DAC value to write
+ */
 exports.setDac = function (keyAddress,value){
-    // keyAddress is midi value of the note
     var localKey = keyAddress-module_base;
     if ((localKey > module_size * modules_connected) || (keyAddress < module_base)){
         throw "Error: key ({0}) out of range".format(keyAddress);
@@ -103,42 +117,86 @@ exports.setDac = function (keyAddress,value){
     rpio.spiWrite(spiCmdBuffer, spiCmdBuffer.length);
     rpio.write(board_enables[moduleAddress], rpio.HIGH);
 };
-
-function velocityToDac(key,velocity,calMap){
-    // maps the velocity value from MIDI to a 10-bit value we write to the DAC
-    // TODO: finalize which velocity format to use (general MIDI 0 to 128, or tonejs 0 to 1)
-    var dacvalue;
+/** maps the velocity value from MIDI to a 10-bit value we write to the DAC
+ * @param noteObj -
+ */
+function velocityToDac(noteObj){
+    const dacMin = 0;
+    const dacMax = 1023;
+    const velocityMin = 0;
+    const velocityMax = 1;
+    // map velocity to DAC, taking into account that key's weight calibration value
+    return dacMin + ((dacMax - dacMin)*noteObj.velocity_value) + noteObj.weightCal; // simplified, premature optimization :^)
+    //return dacMin + ((dacMax - dacMin)/(velocityMax- velocityMin))*(noteObj.velocity_value - velocityMin) + noteObj.weightCal; // proper way if velocity scale changes
 
 }
 
+/**
+ * Communicates the state of the piano keys (on/off, velocity) to the hardware
+ * @param {Loki} stateDB - database containing the relation of the piano hardware state
+ * @param {float} currentTime
+ */
 function transmitState(stateDB, currentTime){
-    // TODO: implement the pseudocode in Drive
+    // TODO: Work with Kevin & get the names right
+    //Obtain the keys that should be on
+    var pianoState = stateDB.getCollection("pianostate").getDynamicView("activeKeys").data();
+    var notesToEnable = [];
+    var key = null;
+    for (var i = 0;i<pianoState.length;i++){ // for each key on the piano:
+        key = pianoState[i];
+        // if the note's velocity changed, write to the DAC
+        if (key.velocityValue !== key.velocityPrevious){
+            setDac(key.keyNumber,velocityToDac(key));
+            key.velocityPrevious = key.velocityValue;
+        }
+        // activate key, only if has not been on for longer than the time limit
+        if (key.onTime + solenoid_on_time_limit > currentTime){
+            notesToEnable.push(key.keyNumber);
+        }
+        // clear the key here. if the key should remain on, the update function will set it again next cycle.
+        // On the upper level, we explicity send "which keys should be on now"; this reset allows us to
+        //   assume that the notes we don't specify are implicitly powered off.
+        key.noteOn = false;
+    }
+    // write to the shift register chain
+    setKeyEnables(notesToEnable);
 
 }
 
-function genCalMap(){
+/**
+ * provides an adjustment value for each key to compensate its weight
+ * @param {int} size - number of keys
+ * @returns {Array[]}
+ */
+exports.genCalMap = function (size){
     // this function should be edited to calibrate the volumes of keys with different weights
     // procedure for this will likely be plotting velocity/measured loudness in excel and fitting an equation
-
+    if (size === null){
+        size = modules_connected*module_size;
+    }
     // set this to 1 if even midi-numbered notes will be played with the heavier (extended) plungers
     const evenNotesHeavy = 0;
 
-    var calMap = new Array(modules_connected*module_size);
-    // populate the map with coefficients that will multiply (or possibly add?) the DAC value
+    var calMap = new Array(size);
+    // populate the map with coefficients that will add (or possibly multiply?) the DAC value
     for (var i=0;i< calMap.length;i++){
         if (i%2 !== evenNotesHeavy){
             // equation for heavier, extended solenoid plungers
-            calMap[i]= 1;
+            calMap[i]= 0;
         }
         else {
             // equation for lighter, top-rail solenoid plungers
-            calMap[i]= 1;
+            calMap[i]= 0;
         }
     }
-}
+    return calMap;
+};
 
-exports.setKeyEnables = function (note_array) {
-    // currently expecting an array of notes (midi numbered) that should be enabled on this following cycle.
+/**
+ * Performs serial write to shift register(s), turning on the specified solenoids
+ * @param {Array} noteArray - array containing the MIDI-numbered keys that will be activated
+ */
+exports.setKeyEnables = function (noteArray) {
     var i;
     var localKey = 0;   // used for "key - base"
 
@@ -147,18 +205,17 @@ exports.setKeyEnables = function (note_array) {
     for (i=0;i<modules_connected;i++) enableBitstream[i]=0;
 
     // iterate through the notes that need to be on, set bits that will be transmitted
-    for (i = 0;i<note_array.length;i++){
-        localKey = note_array[i] - module_base;
+    for (i = 0;i<noteArray.length;i++){
+        localKey = noteArray[i] - module_base;
         //console.log(localKey);
-        if ((localKey > module_size * modules_connected) || (note_array[i] < module_base)){
-            throw "Error: key ({0}) out of range".format(note_array[i]);
+        if ((localKey > module_size * modules_connected) || (noteArray[i] < module_base)){
+            throw "Error: key ({0}) out of range".format(noteArray[i]);
         }
         // set the bit; our shift register chain receives the highest note first (and we transmit MSB first)
         enableBitstream[modules_connected-1-(Math.floor(localKey/8))] |= 1 << localKey%8; // 8 is just byte size here, no magic.
     }
 
     // ready the shift register, and Transmit!
-    rclkFallingEdge();
     var keyEnableBuffer = new Buffer(enableBitstream);
     rpio.spiWrite(keyEnableBuffer, keyEnableBuffer.length);
     // enables sent, push data to output
